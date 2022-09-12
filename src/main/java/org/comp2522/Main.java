e05d292ad7c5be479750e3c4b4e8675e7e66683e
Nash Baek <nash4comp@gmail.com>
2022-09-11 21:25:04 -0700

    Adopted checkstyle.

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,10 @@
 package org.comp2522;
 
+/**
+ * My first tutorial for comp2522 lab01.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, 2522 world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, 2522 world!");
+  }
 }
